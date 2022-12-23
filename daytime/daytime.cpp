365ad53d2813395a78a91de4559d04f7bc0b1c1a
Vadimpon <113329629+Vadimpon@users.noreply.github.com>
2022-12-23 17:12:39 +0300

    Update daytime.cpp

--- daytime/daytime.cpp
+++ daytime/daytime.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <netinet/in.h> 
 #include <arpa/inet.h>
+#define port 13
+#define address "172.16.40.1"
 using namespace std;
 void Exception(const string & why, const int exitCode ) // Исключения (возможные ошибки завершение программы) 
 {
@@ -19,8 +21,8 @@ int main()
     selfAddr->sin_addr.s_addr = 0;  
     sockaddr_in * remoteAddr = new (sockaddr_in);// структура с адресом сервера
     remoteAddr->sin_family = AF_INET;     // интернет протокол IPv4
-    remoteAddr->sin_port = htons(7);  // порт 7
-    remoteAddr->sin_addr.s_addr = inet_addr("95.152.62.42"); //  адрес 
+    remoteAddr->sin_port = htons(13);  // порт 13
+    remoteAddr->sin_addr.s_addr = inet_addr("127.0.0.1"); //  адрес 
     char *bufer = new char[1024]; // буфер для передачи и приема данных
     strcpy(bufer,"Hello, Penza");   //копируем строку
     int msgLen = strlen(bufer);     //вычисляем длину строки
